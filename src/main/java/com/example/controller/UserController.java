9c07ede7b7691e3dea77a5fcf4e549aa46e3ae5b
tkysk <tkysk-github@example.com>
2016-07-14 19:16:59 +0900

    Update repository

--- src/main/java/com/example/controller/UserController.java
+++ src/main/java/com/example/controller/UserController.java
@@ -3,14 +3,18 @@ package com.example.controller;
 import com.example.model.User;
 import com.example.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.annotation.PostConstruct;
+
+import static com.sun.org.apache.xalan.internal.xsltc.compiler.util.Type.Int;
 
 /**
  * Created by saeki on 2016/07/14.
@@ -21,24 +25,43 @@ public class UserController {
     UserRepository repository;
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String home(@ModelAttribute("formModel")User formData, Model model) {
-        model.addAttribute("msg","this is sample content.");
-        Iterable<User> list = repository.findAll();
-        model.addAttribute("datalist",list);
+    public String home(@ModelAttribute("formModel") User formData, Model model) {
+        model.addAttribute("msg", "this is sample content.");
+//        Iterable<User> list = repository.findAll();
+        Iterable<User> list = repository.findByIdIsNotNullOrderByIdDesc();
+        model.addAttribute("datalist", list);
 
         return "home";
     }
 
-    @RequestMapping(value = "/",method = RequestMethod.POST)
+    @RequestMapping(value = "/", method = RequestMethod.POST)
     @Transactional
-    public String form(@ModelAttribute("formModel")User formData, Model model) {
+    public String form(@ModelAttribute("formModel") User formData, Model model) {
         repository.saveAndFlush(formData);
+        return "redirect:/";
+    }
+
+    @RequestMapping(value = "/edit/{id}", method = RequestMethod.GET)
+    public String editPage(
+            @PathVariable Long id,
+            @ModelAttribute User data,
+            Model model
+    ) {
+        User currentData = repository.findById(id);
+        model.addAttribute("formModel", currentData);
+        return "edit";
+    }
+
+    @RequestMapping(value = "/edit", method = RequestMethod.POST)
+    @Transactional
+    public String update(@ModelAttribute User data, Model model) {
+        repository.saveAndFlush(data);
         return "redirect:/";
     }
 
 
     @PostConstruct
-    public void init(){
+    public void init() {
         User d1 = new User();
         d1.setName("Katsuo");
         d1.setAge(11);
--- src/main/java/com/example/repositories/UserRepository.java
+++ src/main/java/com/example/repositories/UserRepository.java
@@ -4,11 +4,17 @@ import com.example.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 /**
  * Created by saeki on 2016/07/14.
  */
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-
+    public User findById(Long id);
+    public Iterable<User> findByIdIsNotNullOrderByIdDesc();
+    public List<User> findByNameLike(String name);
+    public List<User> findByAgeGreaterThan(Integer age);
+    public List<User> findByAgeBetween(Integer age1, Integer age2);
 }
