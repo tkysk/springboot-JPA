cfa2e83b22e73f62909629b8c7256dac0308b8ea
tkysk <tkysk-github@example.com>
2016-07-15 15:46:13 +0900

    add validation

---
+++ src/main/java/com/example/Annotation/Phone.java
@@ -0,0 +1,7 @@
+package com.example.Annotation;
+
+/**
+ * Created by saeki on 2016/07/15.
+ */
+public @interface Phone {
+}
---
+++ src/main/java/com/example/Validator/PhoneValidator.java
@@ -0,0 +1,7 @@
+package com.example.Validator;
+
+/**
+ * Created by saeki on 2016/07/15.
+ */
+public class PhoneValidator {
+}
